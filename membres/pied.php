dd1dca670cc18239fbf3d107f29bd6a182e96e50
Yanis Madani <madaniyanis@gmail.com>
2021-04-01 16:33:30 +0200

    Changement du pied.php

--- membres/pied.php
+++ membres/pied.php
@@ -1,9 +1,57 @@
 
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>			
 		<script type="text/javascript" src="script.js"></script>
-		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-		<script>
-  AOS.init();
-</script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+             AOS.init();
+        </script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>			
 </body>
+
+
+<footer class="bg-dark text-center text-white">
+  <div class="container p-4">
+    <section class="mb-4">
+      <a class="btn btn-outline-light btn-floating m-1" href="index.php" role="button"
+        ><i class="">Contact</i
+      ></a>
+      <a class="btn btn-outline-light btn-floating m-1" href="" role="button"
+        ><i class=""></i
+      ></a>
+    </section>
+    <section class="">
+      <form action="">
+        <div class="row d-flex justify-content-center">
+          <div class="col-auto">
+            <p class="pt-2">
+              <strong>Inscrivez vous à notre newsletter</strong>
+            </p>
+          </div>
+          <div class="col-md-5 col-12">
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="email" class="form-control" placeholder="renseignez votre adresse email"/>
+              <label class="form-label" for="email">Adresse Email</label>
+            </div>
+          </div>
+          <div class="col-auto">
+            <button type="submit" class="btn btn-outline-light mb-4">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </form>
+    </section>
+    <section class="mb-4">
+      <p>
+      Nous sommes un groupe aux valeurs de la mère patrie, nous vous patageons des livre libre de droit dans la langues de molière dans le but d'embellir vos journée d'une petite lecture au coin du feu.
+      Dans le respect des lois anti-social des gouvernements capitaliste légal et pratique pour petits et grands.
+      Amusez vous et découvrez toutes nos offres!
+      </p>
+    </section>
+        </div>
+      </div>
+    </section>
+  </div>
+</footer>
 </html>
