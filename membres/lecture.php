727127365a6d6254cf11dc63cf3132b7699d583a
Yanis Madani <madaniyanis@gmail.com>
2022-03-31 17:35:01 +0200

    Changement modele/lecture

--- membres/lecture.php
+++ membres/lecture.php
@@ -5,7 +5,6 @@ require_once 'entete.php';
 if(isset($_GET["id"]) &&!empty($_GET['id'])){
     $idLivre = $_GET["id"];
     $lectures = new Lecture($idLivre);
-    var_dump($lectures);
 }else{
     header("index.php");
 }
